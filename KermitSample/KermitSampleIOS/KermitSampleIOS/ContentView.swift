c1ca92c1a7253d6ba2e91cdfabe8b90024db481b
Kevin Schildhorn <kevins@touchlab.co>
2020-04-16 16:59:28 -0400

    Revert "Adding CrashKiOS Logger"
    This reverts commit deab1ef0ce02e3b4f9be1744bad2f9c00774dc33.

--- KermitSample/KermitSampleIOS/KermitSampleIOS/ContentView.swift
+++ KermitSample/KermitSampleIOS/KermitSampleIOS/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    let kermit = KermitKermit(loggerList: [OSLogLogger(), UtilKt.getNSLogger(), CrashKiOSLogger()])
+    let kermit = KermitKermit(loggerList: [OSLogLogger(), UtilKt.getNSLogger()])
     let common: SampleCommon
     let cb = CrashBot()
 
