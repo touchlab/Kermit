deab1ef0ce02e3b4f9be1744bad2f9c00774dc33
Kevin Schildhorn <kevins@touchlab.co>
2020-04-16 12:02:00 -0400

    Adding CrashKiOS Logger

--- KermitSample/KermitSampleIOS/KermitSampleIOS/AppDelegate.swift
+++ KermitSample/KermitSampleIOS/KermitSampleIOS/AppDelegate.swift
@@ -16,9 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        CrashIntegrationKt.crashInit(handler: KermitCrashHandler())
-        
         return true
     }
 
