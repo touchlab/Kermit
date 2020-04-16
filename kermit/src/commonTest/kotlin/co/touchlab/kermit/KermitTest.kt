dd0f7c15f53e8941474997a06ddeed0cb70adc5a
Kevin Schildhorn <kevins@touchlab.co>
2020-04-16 16:35:08 -0400

    Adding Default Tag for Kermit (#16)
    * Adding Default Tag for Kermit
    
    * Moved Default tag to constructor
    
    * Making tags non-optional
    
    * Cleaning up testing
    
    * Removing Android Kermit Test
    
    * renaming test
    
    * Removing JUnit references

--- kermit/src/commonTest/kotlin/co/touchlab/kermit/KermitTest.kt
+++ kermit/src/commonTest/kotlin/co/touchlab/kermit/KermitTest.kt
@@ -1,7 +1,9 @@
 package co.touchlab.kermit
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
 class KermitTest {
     @Test
@@ -11,10 +13,30 @@ class KermitTest {
         assertTrue(logged)
     }
 
+    @Test
+    fun testingDefaultTag(){
+        val kermit = Kermit(testLogger)
+        val kermitWithTag = kermit.withTag("My Custom Tag")
+
+        kermit.i("MainActivity") { "onCreate" }
+        assertEquals(currentTag,"MainActivity")
+        kermit.d { "Log Without Tag (Original Kermit)" }
+        assertEquals(currentTag,"Kermit")
+        kermitWithTag.d { "Log Without Tag (Kermit With Tag)" }
+        assertEquals(currentTag,"My Custom Tag")
+        kermit.d("Tag") { "Log WITH Tag (Original Kermit" }
+        assertEquals(currentTag,"Tag")
+        kermit.d { "Log Without Tag (Original Kermit)" }  // Ensuring first Kermit isn't affected by withTag
+        assertEquals(currentTag,"Kermit")
+    }
+
     var logged = false
+    var currentTag:String = ""
+
     private val testLogger = object : Logger() {
-        override fun log(severity: Severity, message: String, tag: String?, throwable: Throwable?) {
+        override fun log(severity: Severity, message: String, tag: String, throwable: Throwable?) {
             logged = true
+            currentTag = tag
         }
     }
 }
