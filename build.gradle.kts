e8233601fc1689633a2d0a7cc679760dfef02d6f
Sam Hill <sam@touchlab.co>
2020-05-29 16:52:16 -0400

    Deploy from CI (#57)
    * Deploy pipeline
    
    * Fix buildscript
    
    * Use signing key

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,53 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-allprojects{
-    repositories{
+allprojects {
+    repositories {
         mavenLocal()
         mavenCentral()
         google()
         jcenter()
+    }
+
+    tasks.register("publishMac") {
+        if (tasks.findByName("publish") != null) {
+            dependsOn("publishKotlinMultiplatformPublicationToMavenRepository",
+                "publishMetadataPublicationToMavenRepository",
+                "publishJvmPublicationToMavenRepository",
+                "publishAndroidDebugPublicationToMavenRepository",
+                "publishAndroidReleasePublicationToMavenRepository",
+                "publishAndroidNativeArm32PublicationToMavenRepository",
+                "publishAndroidNativeArm64PublicationToMavenRepository",
+                "publishAndroidNativeX64PublicationToMavenRepository",
+                "publishAndroidNativeX86PublicationToMavenRepository",
+                "publishIosArm32PublicationToMavenRepository",
+                "publishIosArm64PublicationToMavenRepository",
+                "publishIosX64PublicationToMavenRepository",
+                "publishMacosX64PublicationToMavenRepository",
+                "publishJsPublicationToMavenRepository",
+                "publishWatchosArm32PublicationToMavenRepository",
+                "publishWatchosArm64PublicationToMavenRepository",
+                "publishWatchosX86PublicationToMavenRepository",
+                "publishTvosArm64PublicationToMavenRepository",
+                "publishTvosX64PublicationToMavenRepository",
+                "publishLinuxArm32HfpPublicationToMavenRepository",
+                "publishLinuxArm64PublicationToMavenRepository",
+                "publishLinuxX64PublicationToMavenRepository",
+                "publishWasm32PublicationToMavenRepository")
+        }
+    }
+
+    tasks.register("publishWindows") {
+        if (tasks.findByName("publish") != null) {
+            dependsOn("publishMingwX64PublicationToMavenRepository",
+                "publishMingwX86PublicationToMavenRepository")
+        }
+    }
+
+    tasks.register("publishLinux") {
+        if (tasks.findByName("publish") != null) {
+            dependsOn("publishLinuxMips32PublicationToMavenRepository",
+                "publishLinuxMipsel32PublicationToMavenRepository")
+        }
     }
 }
