f6f7b0895e2a99aa47a9248e8c0e01939bd9becb
Mayank Kharbanda <mayankmkh@gmail.com>
2021-05-29 23:08:27 +0530

    Use view binding in fragments in android sample

--- KermitSample/app/src/main/java/co/touchlab/KermitSample/FirstFragment.kt
+++ KermitSample/app/src/main/java/co/touchlab/KermitSample/FirstFragment.kt
@@ -11,30 +11,21 @@
 package co.touchlab.KermitSample
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import co.touchlab.KermitSample.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+class FirstFragment : Fragment(R.layout.fragment_first) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        val binding = FragmentFirstBinding.bind(view)
+        binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
