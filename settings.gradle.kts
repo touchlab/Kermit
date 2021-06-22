ab466c0b2df1dacea02977792ff3445fb4bfe30a
Kevin Galligan <kgalligan@gmail.com>
2021-06-22 13:31:25 -0400

    External Kotlin Config (#85)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,10 @@ pluginManagement {
         gradlePluginPortal()
         maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
     }
+    val KOTLIN_VERSION: String by settings
+    plugins {
+        kotlin("multiplatform") version KOTLIN_VERSION
+    }
 }
 
 enableFeaturePreview("GRADLE_METADATA")
