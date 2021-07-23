6e1babeb26c4d2170a667d5c48f7d0096b2f48c6
Kevin Galligan <kgalligan@gmail.com>
2021-07-23 11:21:27 -0400

    Performance experiments

--- kermit/src/commonMain/kotlin/co/touchlab/kermit/Kermit.kt
+++ kermit/src/commonMain/kotlin/co/touchlab/kermit/Kermit.kt
@@ -13,8 +13,16 @@ package co.touchlab.kermit
 class Kermit(
     private val loggerList: List<Logger> = listOf(
         CommonLogger()
-    ), private val defaultTag: String = "Kermit"
+    ), val defaultTag: String = "Kermit"
 ) {
+    val minSeverity:Severity = loggerList.minOfOrNull { when{
+        it.isLoggable(Severity.Verbose) -> Severity.Verbose
+        it.isLoggable(Severity.Debug) -> Severity.Debug
+        it.isLoggable(Severity.Info) -> Severity.Info
+        it.isLoggable(Severity.Warn) -> Severity.Warn
+        it.isLoggable(Severity.Error) -> Severity.Error
+        else -> Severity.Assert
+    } } ?: Severity.Assert
 
     constructor(vararg loggers: Logger) : this(loggers.asList())
     constructor(logger: Logger) : this(listOf(logger))
@@ -23,107 +31,71 @@ class Kermit(
         return Kermit(this.loggerList, defaultTag)
     }
 
-    fun v(tag: String = defaultTag, throwable: Throwable? = null, message: () -> String) {
-        processLog(Severity.Verbose, tag, throwable, message, Logger::v)
+    inline fun v(throwable: Throwable? = null, message: () -> String){
+        if(minSeverity <= Severity.Verbose)
+            log(Severity.Verbose, defaultTag, throwable, message())
     }
 
-    fun v(withMessage: () -> String) {
-        v(message = withMessage)
+    inline fun v(tag:String, throwable: Throwable? = null, message: () -> String){
+        if(minSeverity <= Severity.Verbose)
+            log(Severity.Verbose, tag, throwable, message())
     }
 
-    fun v(withThrowable: Throwable, message: () -> String) {
-        v(throwable = withThrowable, message = message)
+    inline fun d(throwable: Throwable? = null, message: () -> String) {
+        if(minSeverity <= Severity.Debug)
+            log(Severity.Debug, defaultTag, throwable, message())
     }
 
-    fun v(withTag: String, message: () -> String) {
-        v(tag = withTag, message = message)
+    inline fun d(tag:String, throwable: Throwable? = null, message: () -> String){
+        if(minSeverity <= Severity.Debug)
+            log(Severity.Debug, tag, throwable, message())
     }
 
-    fun d(tag: String = defaultTag, throwable: Throwable? = null, message: () -> String) {
-        processLog(Severity.Debug, tag, throwable, message, Logger::d)
+    inline fun i(throwable: Throwable? = null, message: () -> String) {
+        if(minSeverity <= Severity.Info)
+            log(Severity.Info, defaultTag, throwable, message())
     }
 
-    fun d(withMessage: () -> String) {
-        d(message = withMessage)
+    inline fun i(tag:String, throwable: Throwable? = null, message: () -> String){
+        if(minSeverity <= Severity.Info)
+            log(Severity.Info, tag, throwable, message())
     }
 
-    fun d(withThrowable: Throwable?, message: () -> String) {
-        d(throwable = withThrowable, message = message)
+    inline fun w(throwable: Throwable? = null, message: () -> String) {
+        if(minSeverity <= Severity.Warn)
+            log(Severity.Warn, defaultTag, throwable, message())
     }
 
-    fun d(withTag: String, message: () -> String) {
-        d(tag = withTag, message = message)
+    inline fun w(tag:String, throwable: Throwable? = null, message: () -> String){
+        if(minSeverity <= Severity.Warn)
+            log(Severity.Warn, tag, throwable, message())
     }
 
-    fun i(tag: String = defaultTag, throwable: Throwable? = null, message: () -> String) {
-        processLog(Severity.Info, tag, throwable, message, Logger::i)
+    inline fun e(throwable: Throwable? = null, message: () -> String) {
+        if(minSeverity <= Severity.Error)
+            log(Severity.Error, defaultTag, throwable, message())
     }
 
-    fun i(withMessage: () -> String) {
-        i(message = withMessage)
+    inline fun e(tag:String, throwable: Throwable? = null, message: () -> String){
+        if(minSeverity <= Severity.Error)
+            log(Severity.Error, tag, throwable, message())
     }
 
-    fun i(withThrowable: Throwable?, message: () -> String) {
-        i(throwable = withThrowable, message = message)
+    inline fun wtf(throwable: Throwable? = null, message: () -> String) {
+        if(minSeverity <= Severity.Assert)
+            log(Severity.Assert, defaultTag, throwable, message())
     }
 
-    fun i(withTag: String, message: () -> String) {
-        i(tag = withTag, message = message)
-    }
-
-    fun w(tag: String = defaultTag, throwable: Throwable? = null, message: () -> String) {
-        processLog(Severity.Warn, tag, throwable, message, Logger::w)
-    }
-
-    fun w(withMessage: () -> String) {
-        w(message = withMessage)
-    }
-
-    fun w(withThrowable: Throwable?, message: () -> String) {
-        w(throwable = withThrowable, message = message)
-    }
-
-    fun w(withTag: String, message: () -> String) {
-        w(tag = withTag, message = message)
-    }
-
-    fun e(tag: String = defaultTag, throwable: Throwable? = null, message: () -> String) {
-        processLog(Severity.Error, tag, throwable, message, Logger::e)
-    }
-
-    fun e(withMessage: () -> String) {
-        e(message = withMessage)
-    }
-
-    fun e(withThrowable: Throwable?, message: () -> String) {
-        e(throwable = withThrowable, message = message)
-    }
-
-    fun e(withTag: String, message: () -> String) {
-        e(tag = withTag, message = message)
-    }
-
-    fun wtf(tag: String = defaultTag, throwable: Throwable? = null, message: () -> String) {
-        processLog(Severity.Assert, tag, throwable, message, Logger::wtf)
-    }
-
-    fun wtf(withMessage: () -> String) {
-        wtf(message = withMessage)
-    }
-
-    fun wtf(withThrowable: Throwable?, message: () -> String) {
-        wtf(throwable = withThrowable, message = message)
-    }
-
-    fun wtf(withTag: String, message: () -> String) {
-        wtf(tag = withTag, message = message)
+    inline fun wtf(tag:String, throwable: Throwable? = null, message: () -> String){
+        if(minSeverity <= Severity.Assert)
+            log(Severity.Assert, tag, throwable, message())
     }
 
     fun log(
         severity: Severity,
         tag: String = defaultTag,
         throwable: Throwable?,
-        message: () -> String
+        message: String
     ) {
         processLog(
             severity,
@@ -135,17 +107,16 @@ class Kermit(
         }
     }
 
-    private inline fun processLog(
+    private fun processLog(
         severity: Severity,
         tag: String,
         throwable: Throwable?,
-        crossinline message: () -> String,
+        message: String,
         loggingCall: Logger.(message: String, tag: String, throwable: Throwable?) -> Unit
     ) {
-        val processedMessage: String by lazy { message() }
         loggerList.forEach {
             if (it.isLoggable(severity)) {
-                it.loggingCall(processedMessage, tag, throwable)
+                it.loggingCall(message, tag, throwable)
             }
         }
     }
