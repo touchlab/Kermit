d0529339a154e743b2ed15a2fa56e16a2fa115e6
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-08-25 09:27:52 +0200

    Ensure accessible classpaths for Android and Kotlin Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,11 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
+plugins {
+    kotlin("multiplatform") apply false
+    id("com.android.library") version "4.1.2" apply false
+}
+
 allprojects {
     repositories {
         mavenLocal()
