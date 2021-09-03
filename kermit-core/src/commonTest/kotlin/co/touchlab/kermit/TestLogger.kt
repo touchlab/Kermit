2d0cb9a3c61a7d5a85aa660d5fb44af5e5b66b8d
Jigar Brahmbhatt <findjigar@gmail.com>
2021-09-03 09:49:54 -0400

    Uncomment and fix tests

--- kermit-core/src/commonTest/kotlin/co/touchlab/kermit/TestLogger.kt
+++ kermit-core/src/commonTest/kotlin/co/touchlab/kermit/TestLogger.kt
@@ -1,11 +1,14 @@
 /*
- * Copyright (c) 2020 Touchlab
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright (c) 2021 Touchlab
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package co.touchlab.kermit
@@ -13,7 +16,11 @@ package co.touchlab.kermit
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class TestLogger(val loggable:Severity = Severity.Verbose) : Logger() {
+class TestConfig(override val minSeverity: Severity,
+                 override val loggerList: List<Logger>,
+                 override val defaultTag: String = ""): KermitConfig
+
+class TestLogger(val loggable:Severity) : Logger() {
     data class LogEntry(
         val severity: Severity,
         val message: String,
