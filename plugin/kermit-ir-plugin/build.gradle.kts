8f9867e2e5d89d71db9be142f9f639f432c31fd2
Kevin Galligan <kgalligan@gmail.com>
2022-04-07 22:56:35 -0400

    Bump compile version

--- plugin/kermit-ir-plugin/build.gradle.kts
+++ plugin/kermit-ir-plugin/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
 
   testImplementation(kotlin("test-junit"))
   testImplementation("org.jetbrains.kotlin:kotlin-compiler-embeddable")
-  testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.1")
+  testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.8")
 }
 
 buildConfig {
