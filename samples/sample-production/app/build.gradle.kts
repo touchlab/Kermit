9191bda7da4cb1f6943e73bf32bd4ec089c4648a
Jigar Brahmbhatt <findjigar@gmail.com>
2023-09-25 14:38:42 -0400

    Fix build

--- samples/sample-production/app/build.gradle.kts
+++ samples/sample-production/app/build.gradle.kts
@@ -35,6 +35,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
     buildFeatures {
         viewBinding = true
         buildConfig = true
