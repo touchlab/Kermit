2999c5942457d4e2065686786e3b310dbee250eb
Jigar Brahmbhatt <findjigar@gmail.com>
2024-01-30 20:27:48 +0530

    Attempt to resolve window machine failure

--- convention-plugins/src/main/kotlin/wasm-setup.gradle.kts
+++ convention-plugins/src/main/kotlin/wasm-setup.gradle.kts
@@ -49,7 +49,9 @@ kotlin {
     }
 }
 
-rootProject.the<NodeJsRootExtension>().apply {
-    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+if (project.findProperty("enableWasm") == "true") {
+    rootProject.the<NodeJsRootExtension>().apply {
+        nodeVersion = "21.0.0-v8-canary202309143a48826a08"
+        nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+    }
 }
