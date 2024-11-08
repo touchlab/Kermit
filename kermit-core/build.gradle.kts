d66ec18d6da1b2699a90b787ad19f38940ceb884
Kevin Schildhorn <kevin.schildhorn@gmail.com>
2024-11-08 11:31:40 -0500

    Update build.gradle.kts

--- kermit-core/build.gradle.kts
+++ kermit-core/build.gradle.kts
@@ -119,15 +119,3 @@ android {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
-
-val mingTask = project.tasks.findByName(":kermit-core:mingwX64MetadataElements")
-val linux64Task = project.tasks.findByName(":kermit-core:linuxX64MetadataElements")
-val linuxArmTask = project.tasks.findByName(":kermit-core:linuxArm64MetadataElements")
-val android86Task = project.tasks.findByName(":kermit-core:androidNativeX86MetadataElements")
-val android64Task = project.tasks.findByName(":kermit-core:androidNativeX64MetadataElements")
-
-mingTask?.dependsOn(tasks.named(":kermit-core:commonizeCInterop"))
-linux64Task?.dependsOn(tasks.named(":kermit-core:commonizeCInterop"))
-linuxArmTask?.dependsOn(tasks.named(":kermit-core:commonizeCInterop"))
-android86Task?.dependsOn(tasks.named(":kermit-core:commonizeCInterop"))
-android64Task?.dependsOn(tasks.named(":kermit-core:commonizeCInterop"))
