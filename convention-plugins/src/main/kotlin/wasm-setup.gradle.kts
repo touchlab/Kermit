dcee646a84a8e57b42e1feab9de8eeeba74a6f92
Jigar Brahmbhatt <findjigar@gmail.com>
2024-11-08 17:36:10 -0500

    Upgrade to Kotlin 2.0.20

--- convention-plugins/src/main/kotlin/wasm-setup.gradle.kts
+++ convention-plugins/src/main/kotlin/wasm-setup.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 /*
  * Copyright (c) 2024 Touchlab
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
@@ -8,10 +10,6 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
-
 plugins {
     kotlin("multiplatform")
 }
@@ -19,20 +17,10 @@ plugins {
 kotlin {
     val wasmEnabled = project.findProperty("enableWasm") == "true"
     if (wasmEnabled) {
-        @OptIn(ExperimentalWasmDsl::class)
+        @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
         wasmJs {
-            browser {
-                commonWebpackConfig {
-                    devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                        static = (static ?: mutableListOf()).apply {
-                            add(project.rootDir.path)
-                        }
-                    }
-                }
-            }
+            browser()
             nodejs()
-            d8()
-            binaries.executable()
         }
     }
 
@@ -42,16 +30,15 @@ kotlin {
             group("jsAndWasmJs") {
                 withJs()
                 if (wasmEnabled) {
-                    withWasm()
+                    withWasmJs()
                 }
             }
         }
     }
 }
 
-if (project.findProperty("enableWasm") == "true") {
-    rootProject.the<NodeJsRootExtension>().apply {
-        nodeVersion = "21.0.0-v8-canary202309143a48826a08"
-        nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+tasks.withType(KotlinCompilationTask::class).configureEach {
+    compilerOptions {
+        freeCompilerArgs.add("-Xexpect-actual-classes")
     }
 }
--- samples/sample/shared/build.gradle.kts
+++ samples/sample/shared/build.gradle.kts
@@ -8,7 +8,6 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -45,7 +44,7 @@ kotlin {
     js {
         browser()
     }
-    @OptIn(ExperimentalWasmDsl::class)
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
     wasmJs {
         browser()
     }
